cc9d82ff6e9333cb5e080cda682346733ba7e3ad
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-04-30 12:07:15 -0300

    Utilización del Y, O y Distinto

---
+++ src/ejercicio10.ts
@@ -0,0 +1,16 @@
+let numero1: number = Number(prompt("ingrese un numero"));
+if (numero1 % 2 === 0 && numero1 > 6) {
+  console.log("Verdadero");
+} else {
+  console.log("Falso");
+}
+if (numero1 >= 5 || numero1 < 2) {
+  console.log("verdadero");
+} else {
+  console.log("falso");
+}
+if (!(numero1 < 5)) {
+  console.log("verdadero");
+} else {
+  console.log("falso");
+}
