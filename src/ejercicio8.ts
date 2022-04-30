12793f611799f97abf43426e65265598c032b36e
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-04-30 12:04:23 -0300

    Medir altura para subir al juego

---
+++ src/ejercicio8.ts
@@ -0,0 +1,5 @@
+let alturaPersona: number = Number(prompt("ingrese la altura"));
+if (alturaPersona <= 1.3) {
+  console.log("Lo sentimos!!!!,No puede subirse a la montaña rusa");
+}
+console.log("Bienvenido!!!, puede subirse a la montaña rusa");
