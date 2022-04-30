fa284985e73cb19e6145a4bfc40da75238651e53
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-04-30 11:58:28 -0300

    Elección de camino a tomar

---
+++ src/ejercicio6.ts
@@ -0,0 +1,6 @@
+let numero1: number = Number(prompt("ingrese un numero"));
+if (numero1 > 50) {
+  console.log("debes tomar el camino a la derecha");
+} else {
+  console.log("debes tomar el camino a la izquierda");
+}
