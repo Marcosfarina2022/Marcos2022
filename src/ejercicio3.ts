550a632a0b2cfd1f787d1ac64ee96ba2a64ff12f
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-04-30 11:47:20 -0300

    Utilizamos el prompt para mostrar una suma

---
+++ src/ejercicio3.ts
@@ -0,0 +1,5 @@
+//Este programa suma dos números ingresados por pantalla utilizando el prompt
+let numero1: number = Number(prompt("ingrese el número 1"));
+let numero2: number = Number(prompt("ingrese el número 2"));
+let resultado: number = numero1 + numero2;
+console.log(resultado);
