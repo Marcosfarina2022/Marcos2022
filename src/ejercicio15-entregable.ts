282db9e2237d38336086aad7c0f9d2a656bd4db0
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-02 10:08:19 -0300

    suma completa de números comenzando por un mayor y luego por un menor

--- src/ejercicio15-entregable.ts
+++ src/ejercicio15-entregable.ts
@@ -1,9 +1,16 @@
+//Programa que suma todos los números comenzando por un mayor y comenzando por un menor
 let numero1: number = Number(prompt("Ingrese el primer número"));
 let numero2: number = Number(prompt("Ingrese un segundo número"));
 let primero, suma: number;
 primero = numero1;
 suma = 0;
-for (let primero: number = numero1; primero >= numero2; primero--) {
-  suma = suma + primero;
+if (numero1 > numero2) {
+  for (let primero: number = numero1; primero >= numero2; primero--) {
+    suma = suma + primero;
+  }
+} else {
+  for (let primero: number = numero1; primero <= numero2; primero++) {
+    suma = suma + primero;
+  }
 }
 console.log("El resultado de la suma es", suma);
