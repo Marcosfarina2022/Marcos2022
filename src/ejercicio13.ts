ef0e08a58cd58a0faeb8cd74f903ded4a8c7ec8b
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-01 22:09:57 -0300

    Ingresar un número y verificar si es par o inpar y mayor que 0

---
+++ src/ejercicio13.ts
@@ -0,0 +1,9 @@
+let numero: number = Number(prompt("Ingrese un número"));
+while (numero <= 0) {
+  let numero: number = Number(prompt("Ingrese otro número distinto a 0"));
+}
+if (numero % 2 === 0) {
+  console.log("El numero ingresado es par");
+} else {
+  console.log("El numero ingresado es inpar");
+}
