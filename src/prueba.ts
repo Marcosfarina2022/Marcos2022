048cbf1c38d8528c388d805ef893cdcab38e6616
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-02 23:05:11 -0300

    promedio de 10 notas

--- src/prueba.ts
+++ src/prueba.ts
@@ -1,16 +1,11 @@
 //Programa que suma todos los números comenzando por un mayor y comenzando por un menor
 let numero1: number = Number(prompt("Ingrese el primer número"));
 let numero2: number = Number(prompt("Ingrese un segundo número"));
-let primero, suma: number;
-primero = numero1;
+let suma: number;
+let auxiliar: number = numero1;
+numero2 = auxiliar;
 suma = 0;
-if (numero1 >= numero2) {
-  for (let primero: number = numero1; primero >= numero2; primero--) {
-    suma = suma + primero;
-  }
-} else {
-  for (let primero: number = numero1; primero <= numero2; primero++) {
-    suma = suma + primero;
-  }
+for (numero2 = numero1; numero2 <= numero1; numero2++) {
+  suma = suma + numero2;
 }
 console.log("El resultado de la suma es", suma);
