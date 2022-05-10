be880987d1325e78c387e096184740b2165a53c4
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-09 21:45:12 -0300

    Cantidad y Distribución de Positivos

---
+++ src/ejercicio20.ts
@@ -0,0 +1,22 @@
+let positivos: number = 0;
+let canTotal: number = 0;
+let porcentaje: number = 0;
+let numero: number = Number(prompt("Ingrese un número"));
+while (numero !== 0) {
+  if (numero > 0) {
+    positivos++;
+  }
+  canTotal++;
+  numero = Number(prompt("Ingrese un valor"));
+}
+
+if (canTotal > 0) {
+  porcentaje = (positivos * 100) / canTotal;
+  console.log(
+    "Cantidad de positivos",
+    positivos,
+    " del ",
+    porcentaje,
+    " % del total "
+  );
+}
