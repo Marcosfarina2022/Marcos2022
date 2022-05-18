0500bd7dd7185f6236fd7764dbd6c36a0a6a83b7
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-18 11:32:17 -0300

    Mostrar apellido y nombre ingresado por teclado utilizando una función

---
+++ src/ejercicio26.ts
@@ -0,0 +1,16 @@
+//Crear un programa con una funcion que reciba por parametro
+//su nombre y apellido y lo imprima por consola.
+function mostrarApellidoyNombre(
+  nombre: string,
+  apellido: string,
+  mostrar: string
+): string {
+  mostrar = nombre + " " + apellido;
+  return mostrar;
+}
+
+let resultado: string = 0;
+let nom: string = prompt("Ingrese su nombre");
+let apell: string = prompt("Ingrese el apellido");
+resultado = mostrarApellidoyNombre(nom, apell, resultado);
+console.log(resultado);
