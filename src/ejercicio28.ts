bac861d4ec30cc63d7d3fa2c1e263ceb7a4c27f5
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-21 18:57:57 -0300

    arreglos, mostrar los números que contiene el arreglo por pantalla

---
+++ src/ejercicio28.ts
@@ -0,0 +1,11 @@
+let arregloNum: number[] = [7];
+arregloNum[0] = 20;
+arregloNum[1] = 14;
+arregloNum[2] = 8;
+arregloNum[3] = 0;
+arregloNum[4] = 5;
+arregloNum[5] = 19;
+arregloNum[6] = 24;
+for (let indice = 0; indice < 7; indice++) {
+  console.log(arregloNum[indice]);
+}
