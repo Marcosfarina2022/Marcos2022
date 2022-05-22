9a405f56f13c80e090bced204be053634b70ea15
Marcos Farina <104174211+Marcosfarina2022@users.noreply.github.com>
2022-05-21 23:24:25 -0300

    imprime por pantalla un arreglo con 5 valores ingresados por teclado

---
+++ src/ejercicio29.ts
@@ -0,0 +1,7 @@
+let numeros: number[] = new Array(5);
+for (let indice = 0; indice < 5; indice++) {
+  numeros[indice] = Number(prompt("Ingrese los valores"));
+}
+for (let indice = 0; indice < 5; indice++) {
+  console.log(numeros[indice]);
+}
